bce07eac56f4e8c22e6ada417eb8d07e3ff16fae
Dimitri Rosenberg <dimitri@postform.io>
2018-12-21 12:31:51 +0300

    changelog production script

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_20_133224) do
+ActiveRecord::Schema.define(version: 2018_12_11_170634) do
 
   create_table "collaborations", force: :cascade do |t|
     t.integer "project_id"
