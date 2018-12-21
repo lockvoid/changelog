73125b819ac0edad066ccacde3ad155f28dd4a47
Dimitri Rosenberg <dimitri@postform.io>
2018-12-21 12:23:49 +0300

    migration

--- db/migrate/20181204192325_create_notices.rb
+++ db/migrate/20181204192325_create_notices.rb
@@ -3,7 +3,7 @@ class CreateNotices < ActiveRecord::Migration[5.2]
     create_table :notices do |t|
       t.string :heading
       t.string :body
-      t.string :trigger_element_css_selector
+      t.string :target_element_selector
       t.references :release, foreign_key: true
 
       t.timestamps
--- db/migrate/20181220133224_rename_trigger_element_css_selector_to_target_element_selector.rb
+++
@@ -1,5 +0,0 @@
-class RenameTriggerElementCssSelectorToTargetElementSelector < ActiveRecord::Migration[5.2]
- def change
-    rename_column :notices, :trigger_element_css_selector, :target_element_selector
-  end
-end
